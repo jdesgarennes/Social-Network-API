49b5bda567ac870499353e05a3144d43fcb5eab4
jdesgarennes <johndesgarennes@gmail.com>
2021-12-20 15:37:00 -0800

    server starts! begin seeding with create API

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const { connect, connection } = require('mongoose');
 connect('mongodb://localhost/socialNetworkDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  //useCreateIndex: true,
+  //useFindAndModify: false,
 });
 
 module.exports = connection;
